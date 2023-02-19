b3f73b138e89552db1f169828e9768b89ac8b633
hayato <ha8to.f513@gmail.com>
2023-02-19 05:17:07 +0000

    借りている本の詳細ページの追加

--- app/controllers/lendings_controller.rb
+++ app/controllers/lendings_controller.rb
@@ -1,16 +1,26 @@
 class LendingsController < ApplicationController
-  before_action :require_signed_in, only: [:index, :show]
-  
+  before_action :require_signed_in?, only: [:index, :show]
+  before_action :redirect_lendings, only: :show
+
   def index
     @lendings = current_user.lendings.not_yet_returned
   end
 
   def show
+    @lending = Lending.find(params[:id])
   end
 
   private
-  #lendingページへのアクセスは、サインインが必要
-  def require_signed_in
+  
+  #lendingへのアクセスは、サインインが必要
+  def require_signed_in?
     redirect_to new_user_session_path if !user_signed_in?
+  end
+
+  # サインインしているユーザーが借りていない本だったら、貸出一覧ページへリダイレクト
+  def redirect_lendings
+    if current_user.lendings.find_by(id: params[:id]).blank?
+      redirect_to lendings_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :books, only: [:index]
   end
+  
   resources :lendings, only: [:index, :show]
 end

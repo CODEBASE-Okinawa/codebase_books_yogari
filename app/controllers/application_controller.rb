b0c8969f6db80098a60bb63a22d44f629a157d80
hayato <ha8to.f513@gmail.com>
2023-02-21 10:49:17 +0000

    admin/ユーザー一覧の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  private
+
+  def check_admin
+    unless current_user&.admin?
+      redirect_to books_path
+    end
+  end
 end

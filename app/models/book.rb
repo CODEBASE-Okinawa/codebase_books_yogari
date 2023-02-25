ca7ee49b7f961f2732858aa410e1dab05327986b
osatoh <sugar961216@gmail.com>
2023-02-25 18:16:33 +0900

    modify book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,22 +1,10 @@
 class Book < ApplicationRecord
+  has_one :lending, -> { where(return_status: false) }
   has_many :lendings, dependent: :destroy
   has_many :reservations
   has_one_attached :image
+
   validates :title, presence: true
 
-  def is_lending?
-    lendings.pluck(:return_status).include?(false) ? true : false
-  end
-
-  def lending_start_at
-    lendings.find_by(return_status: false).created_at
-  end
-
-  def lending_end_at
-    lendings.find_by(return_status: false).return_at
-  end
-
-  def lending_user
-    lendings.find_by(return_status: false).user
-  end
+  delegate :created_at, :return_at, :user, to: :lending
 end

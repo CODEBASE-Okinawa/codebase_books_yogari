8c8cbd98bc998022b501349a2d6dfce3626e9c02
mugiyuki <yukimugikura@gmail.com>
2023-02-26 10:50:09 +0900

    delete show_status from books_controrller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,23 +15,6 @@ class BooksController < ApplicationController
     @reservations = @book.reservations.where("reservation_at >= ?", Time.now).order(reservation_at: :asc)
   end
 
-  def show_status(book)
-    current_user_id = current_user&.id
-    lending_status = book.lend_active.any?{ |lending| lending.user_id == current_user_id }
-    lent_status = book.lend_active
-    reservation_status = book.reservation_active.any?{ |reservation| reservation.user_id == current_user_id }
-
-    if lending_status && current_user_id.present?
-      "lending"
-    elsif reservation_status && current_user_id.present?
-      "reserved"
-    elsif lent_status.present?
-      "lent"
-    else
-      "available"
-    end
-  end
-
   private
 
   def redirect_to_admin_books

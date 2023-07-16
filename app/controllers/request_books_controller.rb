02da17d7c676567a659fb2978df45dd6c6b4d4b4
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-07-16 20:55:33 +0900

    検索結果表示

--- app/controllers/request_books_controller.rb
+++ app/controllers/request_books_controller.rb
@@ -1,2 +1,28 @@
 class RequestBooksController < ApplicationController
+    def search
+        word = URI.encode_www_form_component(params[:word])
+        @result = GoogleBooksApi.search_by_word(word)
+        if @result["totalItems"] == 0
+            flash[:danger] = "本が見つかりませんでした"
+            redirect_to searches_path
+        else
+            @result
+            @items = RequestBook.new
+            render template: "searches/index"
+        end
+    end
+
+    def create
+        @items = RequestBook.new(book_params)
+    end
+
+    def update
+    end
+
+    private
+
+    def book_params
+        params.permit(:title, :author)
+    end
+
 end
--- lib/google_books_api.rb
+++ lib/google_books_api.rb
@@ -5,5 +5,17 @@ module GoogleBooksApi
       response = Faraday.get(url)
       @result = JSON.parse(response.body)
     end
+
+    # def search_by_word(word)
+    #   url = JSON.parse(Net::HTTP.get(URI.parse(Addressable::URI.encode("https://www.googleapis.com/books/v1/volumes?q=title:#{word}&country=JP&maxResults=3"))))
+    #   response = Faraday.get(url)
+    #   @result = response.body
+    # end
+
+    def search_by_word(word)
+      url = "https://www.googleapis.com/books/v1/volumes?q=title:#{word}&maxResults=3"
+      response = Faraday.get(url)
+      @result = JSON.parse(response.body)
+    end
   end
 end

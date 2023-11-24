a38665bc4b03fed8cde276b1720393ebd05a1628
zamami <s1413115@gmail.com>
2023-11-24 16:40:44 +0900

    frontを追加します

---
+++ frontend/app/pages/index.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/books');
+        setBooks(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>本一覧</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            {book.title} - {book.author}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
